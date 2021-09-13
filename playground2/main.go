e0f5f90362e6a7bfa162e3da208b5afd896d0694
Liberbell <liberbell@gmail.com>
2021-09-14 08:34:40 +0900

    main5

--- playground2/main.go
+++ playground2/main.go
@@ -2,12 +2,14 @@ package main
 
 import (
 	"fmt"
-	"math"
 )
 
 func main() {
-	const pi = 3.14
-	pi = 3.14159
-	fmt.Println(math.Pi)
-	fmt.Println(pi)
+	// const pi = 3.14
+	// pi = 3.14159
+	// fmt.Println(math.Pi)
+	// fmt.Println(pi)
+
+	const myConst = 1
+	fmt.Printf("type: %T, value: %v", myConst+1.1, myConst+1.1)
 }

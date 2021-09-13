e835297ae0a348470b96f34c0d6fcb563a89ff43
Liberbell <liberbell@gmail.com>
2021-09-14 08:42:54 +0900

    main10

--- playground2/main.go
+++ playground2/main.go
@@ -4,16 +4,27 @@ import (
 	"fmt"
 )
 
+const (
+	Sunday = iota
+	Monday
+	Thuesday
+	Wednesday
+	Thursday
+	Friday
+	Saturday
+)
+
 func main() {
 	// const pi = 3.14
 	// pi = 3.14159
 	// fmt.Println(math.Pi)
 	// fmt.Println(pi)
 
-	const myConst = setMe()
-	fmt.Printf("type: %T, value: %v\n", myConst+1.1, myConst+1.1)
+	// 	const myConst = setMe()
+	// 	fmt.Printf("type: %T, value: %v\n", myConst+1.1, myConst+1.1)
+	fmt.Println(Sunday, Monday, Thuesday, Wednesday, Thursday, Friday, Saturday)
 }
 
-func setMe() int {
-	return 1
-}
+// func setMe() int {
+// 	return 1
+// }

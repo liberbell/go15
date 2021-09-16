f80cdf967d16021aeacbd992e743c34b71656350
Liberbell <liberbell@gmail.com>
2021-09-16 09:03:31 +0900

    array1

--- playground4/array.go
+++ playground4/array.go
@@ -1,12 +1,17 @@
 package main
 
-import "fmt"
-
 func main() {
 	// var arr [4]int
-	arr := [4]int{1, 2, 3, 4}
-	arr[3] = 10
-	fmt.Println("value of array[1]: ", arr[1])
-	fmt.Println("length of array: ", len(arr))
-	fmt.Println("values: ", arr)
+	// arr := [4]int{1, 2, 3, 4}
+	// arr[3] = 10
+	// fmt.Println("value of array[1]: ", arr[1])
+	// fmt.Println("length of array: ", len(arr))
+	// fmt.Println("values: ", arr)
+
+	var twoDimArr [3][2]int
+	for i := 0; i < 3; i++ {
+		for j := 0; j < 2; j++ {
+			twoDimArr[i][j] = i + j
+		}
+	}
 }

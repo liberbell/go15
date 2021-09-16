7edcaa796b9573d8f5d180c6fc07a0b68099c065
Liberbell <liberbell@gmail.com>
2021-09-16 15:09:46 +0900

    main5

--- playground6/main.go
+++ playground6/main.go
@@ -1,5 +1,7 @@
 package main
 
+import "fmt"
+
 func main() {
 	// var prodPrice map[string]int
 	// fmt.Println(prodPrice)
@@ -16,4 +18,5 @@ func main() {
 	}
 
 	empPrice["turbo widget"] = 100
+	fmt.Println(empPrice)
 }

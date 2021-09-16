4f53060414f78df858f2db79037d25113c2ba108
Liberbell <liberbell@gmail.com>
2021-09-16 09:33:03 +0900

    main10

--- playground5/main.go
+++ playground5/main.go
@@ -11,10 +11,18 @@ func main() {
 	// s1 = append(s1, s1Append...)
 	// fmt.Println("value of s1[1]: ", s1[1])
 
-	s1 := []int{0, 1, 2, 3}
-	s1Copy := make([]int, len(s1))
-	e1 := copy(s1Copy, s1)
-	fmt.Println("length of s1Copy: ", len(s1Copy))
-	fmt.Println("values: ", s1Copy)
-	fmt.Println("number of elements copied: ", e1)
+	// s1 := []int{0, 1, 2, 3}
+	// s1Copy := make([]int, len(s1))
+	// e1 := copy(s1Copy, s1)
+	// fmt.Println("length of s1Copy: ", len(s1Copy))
+	// fmt.Println("values: ", s1Copy)
+	// fmt.Println("number of elements copied: ", e1)
+
+	sl := []int{0, 1, 2, 3, 4, 5}
+	sl1 := sl[2:4]
+	sl2 := sl[:5]
+	sl3 := sl[3:]
+	fmt.Println("values in sl1: ", sl1)
+	fmt.Println("valeus in sl2: ", sl2)
+	fmt.Println("values in sl3: ", sl3)
 }

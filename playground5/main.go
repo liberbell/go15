443398597cc9b3bca5d98cf349abca19c30e5113
Liberbell <liberbell@gmail.com>
2021-09-16 09:23:25 +0900

    main8

--- playground5/main.go
+++ playground5/main.go
@@ -5,11 +5,15 @@ import "fmt"
 func main() {
 	// var s1 []int
 	// s1 := make([]int, 4)
-	s1 := []int{0, 1, 2, 3}
+	// s1 := []int{0, 1, 2, 3}
 	// s1[1] = 10
-	s1Append := []int{4, 5, 6}
-	s1 = append(s1, s1Append...)
+	// s1Append := []int{4, 5, 6}
+	// s1 = append(s1, s1Append...)
 	// fmt.Println("value of s1[1]: ", s1[1])
+
+	s1 := []int{0, 1, 2, 3}
+	s1Copy := make([]int, len(s1))
+	e1 := copy(s1Copy, s1)
 	fmt.Println("values: ", s1)
 	fmt.Println("is zero slice nil?", s1 == nil)
 	fmt.Println("length of s1: ", len(s1))

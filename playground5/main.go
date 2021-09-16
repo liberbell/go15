f9cc4ad3f9f22fd89a3dfd58d312dac991917b80
Liberbell <liberbell@gmail.com>
2021-09-16 09:16:12 +0900

    main4

--- playground5/main.go
+++ playground5/main.go
@@ -4,7 +4,8 @@ import "fmt"
 
 func main() {
 	// var s1 []int
-	s1 := make([]int, 4)
+	// s1 := make([]int, 4)
+	s1 := []int{0, 1, 2, 3}
 	fmt.Println("values: ", s1)
 	fmt.Println("is zero slice nil?", s1 == nil)
 }

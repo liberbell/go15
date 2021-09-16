e5e2927064522f61b4649aef36b934be90363832
Liberbell <liberbell@gmail.com>
2021-09-16 09:21:27 +0900

    main8

--- playground5/main.go
+++ playground5/main.go
@@ -7,7 +7,8 @@ func main() {
 	// s1 := make([]int, 4)
 	s1 := []int{0, 1, 2, 3}
 	// s1[1] = 10
-	s1 = append(s1, 4, 5, 6)
+	s1Append := []int{4, 5, 6}
+	s1 = append(s1, s1Append...)
 	// fmt.Println("value of s1[1]: ", s1[1])
 	fmt.Println("values: ", s1)
 	fmt.Println("is zero slice nil?", s1 == nil)

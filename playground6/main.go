1cdc0c53d88e9f126e5743f0b87b3353a65144c2
Liberbell <liberbell@gmail.com>
2021-09-16 10:03:24 +0900

    main5

--- playground6/main.go
+++ playground6/main.go
@@ -1,17 +1,19 @@
 package main
 
-import (
-	"fmt"
-)
-
 func main() {
-	var prodPrice map[string]int
-	fmt.Println(prodPrice)
+	// var prodPrice map[string]int
+	// fmt.Println(prodPrice)
 	// prodPrice["widget"] = 100
 
-	tempPrice := make(map[string]int)
-	tempPrice["convertible widget"] = 150
-	prodPrice = tempPrice
-	prodPrice["widget"] = 100
-	fmt.Println(prodPrice)
+	// tempPrice := make(map[string]int)
+	// tempPrice["convertible widget"] = 150
+	// prodPrice = tempPrice
+	// prodPrice["widget"] = 100
+	// fmt.Println(prodPrice)
+
+	empPrice := map[string]int{
+		"widget": 75,
+	}
+
+	empPrice["turbo widget"] = 100
 }

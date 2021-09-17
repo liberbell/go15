fa8539bf1674239186700525174abc3c3fc8324a
Liberbell <liberbell@gmail.com>
2021-09-17 15:09:16 +0900

    main6

--- playground7/main.go
+++ playground7/main.go
@@ -7,11 +7,12 @@ func main() {
 	// val := 123
 	// var ptr *int = &val
 	// fmt.Println(ptr, *ptr)
-	var ptr *int = new(int)
-	fmt.Println(ptr, *ptr)
-	*ptr = 234
-	fmt.Println(ptr, *ptr)
+	// var ptr *int = new(int)
+	// fmt.Println(ptr, *ptr)
+	// *ptr = 234
+	// fmt.Println(ptr, *ptr)
 
 	val := 123
+	ptr := &val
 	fmt.Println(ptr, *ptr)
 }

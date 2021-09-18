74be02e49fda1850f8f53a642c35f02890d7bd64
Liberbell <liberbell@gmail.com>
2021-09-18 15:57:10 +0900

    switch2

--- playground7/switchstate.go
+++ playground7/switchstate.go
@@ -3,19 +3,29 @@ package main
 import "fmt"
 
 func main() {
-	workday := 3
-	switch workday {
-	case 1:
-		fmt.Println("Monday")
-	case 2:
-		fmt.Println("Thuesday")
-	case 3:
-		fmt.Println("Wednesday")
-	case 4:
-		fmt.Println("Thursday")
-	case 5:
-		fmt.Println("Friday")
+	// workday := 3
+	// switch workday {
+	// case 1:
+	// 	fmt.Println("Monday")
+	// case 2:
+	// 	fmt.Println("Thuesday")
+	// case 3:
+	// 	fmt.Println("Wednesday")
+	// case 4:
+	// 	fmt.Println("Thursday")
+	// case 5:
+	// 	fmt.Println("Friday")
+	// default:
+	// 	fmt.Println("Take the weekend off")
+	// }
+
+	temp := -10
+	switch {
+	case temp < 0:
+		fmt.Println("Below freezing.")
+	case temp == 0:
+		fmt.Println("At freezing point.")
 	default:
-		fmt.Println("Take the weekend off")
+		fmt.Println("Above freezing.")
 	}
 }

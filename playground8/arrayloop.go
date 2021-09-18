49efb1a3bfa19488436c759dec5624bdd3a7dfa6
Liberbell <liberbell@gmail.com>
2021-09-18 16:46:48 +0900

    arrayloop3

--- playground8/arrayloop.go
+++ playground8/arrayloop.go
@@ -1,10 +1,13 @@
 package main
 
-import "fmt"
-
 func main() {
-	sl := []int{10, 20, 30, 40}
-	for i, value := range sl {
-		fmt.Println(i, value)
+	// sl := []int{10, 20, 30, 40}
+	// for i, value := range sl {
+	// 	fmt.Println(i, value)
+	// }
+	prodPrice := map[string]int{
+		"widget":             75,
+		"turbo widget":       100,
+		"convertible widget": 150,
 	}
 }

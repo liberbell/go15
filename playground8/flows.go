65ac56abba3233a26c341f0266bd123c86b3e2d5
Liberbell <liberbell@gmail.com>
2021-09-18 16:10:03 +0900

    flows3

--- playground8/flows.go
+++ playground8/flows.go
@@ -3,7 +3,7 @@ package main
 import "fmt"
 
 func main() {
-	if score := 99; score < 100 {
+	if score := 1199; score < 100 {
 		fmt.Println("Oops")
 	} else if score > 100 && score < 1000 {
 		fmt.Println("good")

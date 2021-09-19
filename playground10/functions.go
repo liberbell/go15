2dd67d0fd702cd43afc343a97d7d73dd98fdf1ef
Liberbell <liberbell@gmail.com>
2021-09-19 20:46:44 +0900

    functions3

--- playground10/functions.go
+++ playground10/functions.go
@@ -2,12 +2,15 @@ package main
 
 import "fmt"
 
-func add(x int, y int) int {
+var x int = 20
+var y int = 10
+
+func add() int {
 	return x + y
 }
 
 func main() {
-	x := 20
-	y := 10
+	// x := 20
+	// y := 10
 	fmt.Println(add())
 }

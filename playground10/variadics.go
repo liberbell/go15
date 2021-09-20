0ca4a1d7f4c07ba5c441894df81951103f0fd835
Liberbell <liberbell@gmail.com>
2021-09-20 13:00:54 +0900

    variadics4

--- playground10/variadics.go
+++ playground10/variadics.go
@@ -1,12 +1,16 @@
 package main
 
-func val(vals ...int) {
+import "fmt"
+
+func add(vals ...int) {
 	total := 0
 	for _, val := range vals {
 		total += val
 	}
+	fmt.Println(vals, total)
 }
 
 func main() {
-
+	vals := []int{1, 2, 3, 4}
+	add(vals...)
 }

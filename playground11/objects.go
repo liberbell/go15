8855fc252ccff91659a173076211487dba08aa5f
Liberbell <liberbell@gmail.com>
2021-09-21 09:25:46 +0900

    objects3

--- playground11/objects.go
+++ playground11/objects.go
@@ -14,6 +14,14 @@ func (cu *Cube) volume() float64 {
 	return cu.depth * cu.width * cu.height
 }
 
+type Sphere struct {
+	radius float64
+}
+
+func (sp *Sphere) volume() float64 {
+	return ((4.0 / 3.0) * 3.14 * (sp.radius * sp.radius * sp.radius))
+}
+
 func main() {
 	a
 }
